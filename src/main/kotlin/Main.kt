a3a4b71c3bb74731abcae67f953923f53111a4a3
daniil.mironov <daniil.mironov@team.wrike.com>
2022-01-03 17:08:41 +0300

    init commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,71 @@
+import kotlin.math.abs
+
+const val delta: Double = 0.01
+
+const val yearlyStockMultiplicationPercents: Double = 13.5
+const val yearlyBondsMultiplicationPercents: Double = 6.0
+const val inflationPercents: Double = 2.0
+const val tariff: Double = 0.003
+const val tax: Double = 0.13
+
+
+fun main() {
+    println(calcMonthlyInvestByWantedIncomeAndYearsInvesting(20, 1000.0))
+}
+
+fun calcMonthlyInvestByWantedIncomeAndYearsInvesting(
+    years: Int,
+    wantedMonthlyIncome: Double
+): Double {
+    val neededFunds = calcNeededFundsWithInflation(years, wantedMonthlyIncome)
+    return findMinimizationArgument {
+        abs(calcEndSum(years, it) - neededFunds)
+    }
+}
+
+fun calcNeededFundsWithInflation(years: Int, wantedMonthlyIncomeWithoutInflation: Double): Double {
+    val wantedMonthlyIncomeWithInflation = calcInflatedWantedMonthlyIncome(years, wantedMonthlyIncomeWithoutInflation)
+    return calcNeededFunds(wantedMonthlyIncomeWithInflation)
+}
+
+fun calcInflatedWantedMonthlyIncome(years: Int, wantedMonthlyIncome: Double): Double {
+    var income = wantedMonthlyIncome
+    val inflationMultiplication = 1.0 + inflationPercents / 100.0
+    for (i in 1..years) {
+        income *= inflationMultiplication
+    }
+    return income
+}
+
+fun calcNeededFunds(wantedMonthlyIncome: Double): Double {
+    val wantedYearlyIncomeWithTax = (wantedMonthlyIncome * 12.0) / ((1.0 - tax) * (1.0 - tariff))
+    val yearlyBondsAdding = yearlyBondsMultiplicationPercents / 100.0
+    return wantedYearlyIncomeWithTax / yearlyBondsAdding
+}
+
+fun calcEndSum(years: Int, monthlyInvest: Double): Double {
+    var sum = 0.0
+    val monthsInvesting = years * 12
+    for (i in 1..monthsInvesting) {
+        val monthsLeft = monthsInvesting - i
+        val yearlyMultiplicationPercents = getMultiplicationPercentsByMonthsLeft(monthsLeft)
+        sum = calcSumNextMonth(sum, monthlyInvest, yearlyMultiplicationPercents)
+    }
+    return sum
+}
+
+fun calcSumNextMonth(currentSum: Double, monthlyInvest: Double, yearlyMultiplicationPercents: Double): Double {
+    val monthlyMultiplication = 1.0 + (yearlyMultiplicationPercents / 12) / 100
+    return monthlyMultiplication * currentSum + monthlyMultiplication * (monthlyInvest * (1.0 - tariff))
+}
+
+fun getMultiplicationPercentsByMonthsLeft(monthsLeft: Int): Double {
+    val deductionMonths = 5 * 12
+    return if (monthsLeft >= deductionMonths) {
+        yearlyStockMultiplicationPercents
+    } else {
+        val deductValue =
+            (yearlyStockMultiplicationPercents - yearlyBondsMultiplicationPercents) / deductionMonths.toDouble()
+        yearlyBondsMultiplicationPercents + deductValue * monthsLeft
+    }
+}
---
+++ src/main/kotlin/OptimizationFunctions.kt
@@ -0,0 +1,24 @@
+import kotlin.math.abs
+
+const val goldenSection: Double = 1.618
+
+fun findMinimizationArgument(minimizedFunction: (Double) -> Double): Double {
+    return minimizationHelper(minimizedFunction, -1000000.0, 1000000.0)
+}
+
+private fun minimizationHelper(minimizedFunction: (Double) -> Double, low: Double, high: Double): Double {
+    if (abs(high - low) < delta) {
+        return (high + low) / 2.0
+    }
+    if (low > high) throw IllegalArgumentException("Optimization error: low must be less then high!")
+    val step = (high - low) / goldenSection
+    val x1 = high - step
+    val x2 = low + step
+    val y1 = minimizedFunction(x1)
+    val y2 = minimizedFunction(x2)
+    return if (y1 >= y2) {
+        minimizationHelper(minimizedFunction, x1, high)
+    } else {
+        minimizationHelper(minimizedFunction, low, x2)
+    }
+}
